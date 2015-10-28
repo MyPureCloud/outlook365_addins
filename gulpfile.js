9436e3a60226436bf439b3e97cf0617f53308ab0
Kevin Glinski <kevin.glinski@inin.com>
2015-10-28 16:10:01 -0400

    directory cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var replace = require('gulp-replace');
 var rimraf = require('gulp-rimraf');
 
 gulp.task('bower', function() {
-    return gulp.src('bower_components/**/*.*')
+    return gulp.src('src/bower_components/**/*.*')
         .pipe(gulp.dest('localBuild/bower_components/'))
 });
 
@@ -22,20 +22,20 @@ gulp.task('scripts', function() {
     ));
 */
 
-  return gulp.src('public/scripts/**/*.js')
+  return gulp.src('src/scripts/**/*.js')
     //.pipe(jshint('.jshintrc'))
     //.pipe(jshint.reporter('default'))
     .pipe(gulp.dest('localBuild/scripts'));
 });
 
 gulp.task('images', function() {
-  return gulp.src('public/images/**/*')
+  return gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('localBuild/images'));
 });
 
 gulp.task('css', function() {
-  return gulp.src('public/styles/**/*')
+  return gulp.src('src/styles/**/*')
     .pipe(gulp.dest('localBuild/styles'));
 });
 
@@ -46,13 +46,13 @@ gulp.task('clean', function() {
 
 gulp.task('html', function() {
 
-  return gulp.src('public/**/*.html')
+  return gulp.src('src/**/*.html')
       .pipe(replace(/foo/g, 'bar'))
     .pipe(gulp.dest('localBuild'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./public/*.*', ['default']);
+    gulp.watch('./src/*.*', ['default']);
 });
 
 gulp.task('default', ['clean', 'bower', 'scripts','images', 'css','html']);
--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var https = require("https");
 var express = require('express');
 var app = express();
 
+var logger = function(req, res, next) {
+    console.log(req.originalUrl);
+    next(); // Passing the request to the next handler in the stack.
+}
 
-app.use(express.static(__dirname + "/public"));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(logger);
+app.use(express.static(__dirname + "/src"));
 
 app.get("/test.html", function(req, res){
     res.redirect("directory.html");
@@ -16,7 +20,7 @@ app.get("/test.html", function(req, res){
 var sslOptions = {
     key: fs.readFileSync('ssl/localhost.key'),
     cert: fs.readFileSync('ssl/localhost.crt'),
-    ca: fs.readFileSync('ca.crt'),
+    ca: fs.readFileSync('ssl/ca.crt'),
     requestCert: true,
     rejectUnauthorized: false
 };
