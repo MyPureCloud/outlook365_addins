29db4c74e9172d1946160b21182bba73918b545e
Kevin Glinski <kevin.glinski@inin.com>
2015-11-17 12:05:43 -0500

    image path fix

--- gulpfile.js
+++ gulpfile.js
@@ -57,8 +57,8 @@ gulp.task('manifest', function () {
             }
 
             manifest.indexFiles.push({
-                "url": "/github-outlook365addins/images/setup/purecloud.jpg",
-                "file": ".//purecloud.jpg"
+                "url": "/github-outlook365addins/images/purecloud.jpg",
+                "file": "./purecloud.jpg"
             });
 
             fs.writeFileSync("localBuild/manifest.json", JSON.stringify(manifest, null, " "));
